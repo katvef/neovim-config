2e967168080664e59f7263650f05a6aee8c2e8e4
Elmortti <eliot.fredman@gmail.com>
2025-04-21 01:50:34 +0300

    Modified telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,16 +22,19 @@ return {
 			}
 		})
 		require("telescope").load_extension("aerial")
+		local map = vim.keymap.set
 		local builtin = require("telescope.builtin")
 		local theme = require("telescope.themes")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>pf', function() builtin.find_files(theme.get_dropdown()) end, { desc = 'Telescope find files' })
-		vim.keymap.set("n", "<leader>fg", function() builtin.live_grep(theme.get_dropdown()) end, { desc = 'Telescope live grep' })
-		vim.keymap.set("n", "<leader>fb", function() builtin.buffers(theme.get_dropdown()) end, { desc = 'Telescope buffers' })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = 'Telescope help tags' })
-		vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = 'Telescope fuzzy search' })
-		vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
-		vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
+
+		map("n", "<leader>ff", builtin.find_files, { desc = 'Telescope find files (no theme)' })
+		map('n', '<leader>fp', function() builtin.find_files(theme.get_dropdown()) end, { desc = 'Telescope find files' })
+		map("n", "<leader>fg", function() builtin.live_grep(theme.get_dropdown()) end, { desc = 'Telescope live grep' })
+		map("n", "<leader>fb", function() builtin.buffers(theme.get_dropdown()) end, { desc = 'Telescope buffers' })
+		map("n", "<leader>fs", function() builtin.treesitter(theme.get_dropdown()) end, { desc = 'Telescope buffers' })
+		map("n", "<leader>fh", builtin.help_tags, { desc = 'Telescope help tags' })
+		map("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = 'Telescope fuzzy search' })
+		map('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
+		map('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
 	end,
-	tag = "0.1.8"
+	-- tag = "0.1.8"
 }
