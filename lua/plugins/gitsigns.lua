f7a3c1cc40ec8ff902408f9e36ea67f596772962
Elmortti <eliot.fredman@gmail.com>
2025-04-21 04:10:45 +0300

    Further configured gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
 	'lewis6991/gitsigns.nvim',
 	config = function()
 		require("gitsigns").setup({
-			signs = {
+			signs                   = {
 				add          = { text = '+' },
 				change       = { text = '~' },
 				delete       = { text = '_' },
@@ -10,7 +10,7 @@ return {
 				changedelete = { text = '/' },
 				untracked    = { text = '┆' },
 			},
-			signs_staged = {
+			signs_staged            = {
 				add          = { text = '+' },
 				change       = { text = '~' },
 				delete       = { text = '_' },
@@ -18,13 +18,25 @@ return {
 				changedelete = { text = '/' },
 				untracked    = { text = '┆' },
 			},
-			preview_config = {
+			preview_config          = {
 				border = '',
 				style = 'minimal',
 				relative = 'cursor',
 				row = -1,
 				col = 0
 			},
+			linehl                  = false,
+			word_diff               = false,
+			current_line_blame      = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = 'eol',
+				delay = 0,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = false,
+			},
+
 
 			on_attach = function(bufnr)
 				local gitsigns = require("gitsigns")
