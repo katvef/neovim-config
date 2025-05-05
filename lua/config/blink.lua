2dfa913b8d59d9e36f25394d2427371161609f13
Elmortti <eliot.fredman@gmail.com>
2025-05-06 01:57:11 +0300

    Tweaked blink

--- lua/config/blink.lua
+++ lua/config/blink.lua
@@ -1,25 +1,79 @@
 require("blink.cmp").setup({
 	keymap = { preset = "super-tab" },
 	appearance = { nerd_font_variant = "mono" },
-	sources = { default = { "lsp", "snippets", "buffer", "path" } },
+
+	fuzzy = {
+		implementation = "prefer_rust_with_warning",
+		sorts = {
+			"exact",
+			"score",
+			"sort_text",
+		}
+	},
+
+	sources = {
+		default = { "lsp", "snippets", "buffer", "path" },
+		providers = {
+			path = { opts = { get_cwd = function(_) return vim.fn.getcwd() end } },
+			buffer = {
+				-- keep case of first char
+				transform_items = function(a, items)
+					local keyword = a.get_keyword()
+					local correct, case
+					if keyword:match('^%l') then
+						correct = '^%u%l+$'
+						case = string.lower
+					elseif keyword:match('^%u') then
+						correct = '^%l+$'
+						case = string.upper
+					else
+						return items
+					end
+
+					-- avoid duplicates from the corrections
+					local seen = {}
+					local out = {}
+					for _, item in ipairs(items) do
+						local raw = item.insertText
+						if raw:match(correct) then
+							local text = case(raw:sub(1, 1)) .. raw:sub(2)
+							item.insertText = text
+							item.label = text
+						end
+						if not seen[item.insertText] then
+							seen[item.insertText] = true
+							table.insert(out, item)
+						end
+					end
+					return out
+				end
+			}
+		}
+	},
 
 	completion = {
 		documentation = { auto_show = true },
 		keyword = { range = "full" },
+
 		menu = {
-			auto_show = function () return vim.lsp.buf_is_attached() end,
+			auto_show = function()
+				return vim.lsp.buf_is_attached(0) and vim.bo.filetype ~= "markdown"
+			end,
 			draw = { treesitter = { "lsp" } },
 		},
+
 		trigger = {
 			show_on_keyword = true,
 			show_on_trigger_character = true,
 		},
+
 		list = {
 			selection = {
 				preselect = true,
 				auto_insert = true
 			}
 		},
+
 		ghost_text = {
 			enabled = true,
 			show_with_selection = true,
