5f7746767aa9b426f38471ff77a03b85eb12a5c5
Elmortti <eliot.fredman@gmail.com>
2025-05-11 02:00:53 +0300

    Preserve folds
    Added an autocommand to :mkview so folds are preserved

--- lua/elmortti/autocmds.lua
+++ lua/elmortti/autocmds.lua
@@ -11,7 +11,7 @@ autocmd("QuickFixCmdPost", {
 autocmd("TextYankPost", {
 	desc = "Highlight yanked text",
 	group = autogrp("HighlightYank", { clear = true }),
-	callback = function ()
+	callback = function()
 		vim.highlight.on_yank()
 	end
 })
@@ -19,10 +19,34 @@ autocmd("TextYankPost", {
 autocmd("BufEnter", {
 	desc = "Disable tabs in .yuck files",
 	group = autogrp("DisableTabs", { clear = true }),
-	callback = function ()
+	callback = function()
 		if vim.bo.filetype == "yuck" then
 			vim.bo.expandtab = true
 			vim.bo.indentexpr = "lispwords"
 		end
 	end
+})
+
+autogrp("AutoView", { clear = true })
+
+autocmd({ "BufWinLeave", "BufWritePost", "WinLeave" }, {
+	group = "AutoView",
+	callback = function(args)
+		if vim.b[args.buf].view_activated then vim.cmd.mkview { mods = { emsg_silent = true } } end
+	end,
+})
+
+autocmd("BufWinEnter", {
+	group = "AutoView",
+	callback = function(args)
+		if not vim.b[args.buf].view_activated then
+			local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+			local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+			local ignore_filetypes = { "gitcommit", "gitrebase", "jjdescription" }
+			if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) then
+				vim.b[args.buf].view_activated = true
+				vim.cmd.loadview { mods = { emsg_silent = true } }
+			end
+		end
+	end,
 })
