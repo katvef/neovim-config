c6ae8f089b5ebd4ac9a50237f7104c2986ef7348
Elmortti <eliot.fredman@gmail.com>
2025-05-11 22:54:17 +0300

    Added another way to open telescope git files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -40,16 +40,17 @@ return {
 		local theme = require("telescope.themes")
 
 		map("<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		map("<leader>fr", builtin.git_files, { desc = "Telescope git files" })
 		map("<leader>fp", function() builtin.find_files(theme.get_dropdown()) end, { desc = "Telescope find files" })
 		map("<leader>fg", function() builtin.live_grep(theme.get_dropdown()) end, { desc = "Telescope live grep" })
 		map("<leader>fb", function() builtin.buffers(theme.get_dropdown()) end, { desc = "Telescope buffers" })
-		map("<leader>ft", function() builtin.treesitter(theme.get_dropdown()) end, { desc = "Telescope buffers" })
+		map("<leader>ft", function() builtin.treesitter(theme.get_dropdown()) end, { desc = "Telescope treesitter" })
 		map("<leader>fj", builtin.jumplist, { desc = "Telescope jump list" })
 		map("<leader>fo", builtin.grep_string, { desc = "Telescope grep string" })
 		map("<leader>fs", builtin.oldfiles, { desc = "Telescope file history" })
 		map("<leader>fh", builtin.current_buffer_fuzzy_find, { desc = "Telescope fuzzy search" })
 		map("<leader>/", builtin.current_buffer_fuzzy_find, { desc = "Telescope fuzzy search" })
-		map("<C-p>", builtin.git_files, { desc = "Telescope find git files" })
+		map("<C-p>", builtin.git_files, { desc = "Telescope git files" })
 	end,
 	-- tag = "0.1.8"
 }
