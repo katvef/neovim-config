4cf3e1a09e90d425e7abad6ec2694bcbe5cc3010
Elmortti <eliot.fredman@gmail.com>
2025-05-24 01:42:36 +0300

    Removed some comments and changed rip substitute bind
    - Removed documentation comments and type annotations from rip
    substitute config
    - Changed rip susbstitue bind from `<leader>fs` to `<leader>sr`

--- lua/plugins/nvim-rip-substitute.lua
+++ lua/plugins/nvim-rip-substitute.lua
@@ -3,39 +3,37 @@ return {
 		"chrisgrieser/nvim-rip-substitute",
 		cmd = "RipSubstitute",
 		keys = {
-			{ "<leader>fs", function() require("rip-substitute").sub() end, mode = { "n", "x" }, desc = " rip substitute", },
+			{ "<leader>sr", function() require("rip-substitute").sub() end, mode = { "n", "x" }, desc = " rip substitute", },
 		},
-		-- Configuration
 		config = function()
-			-- default settings
 			require("rip-substitute").setup {
+				editingBehavior = { autoCaptureGroups = true },
+				notificationOnSuccess = true,
+
 				popupWin = {
 					title = " rip-substitute",
 					border = "single",
 					matchCountHlGroup = "Keyword",
 					noMatchHlGroup = "ErrorMsg",
 					hideSearchReplaceLabels = false,
-					---@type "top"|"bottom"
 					position = "top",
 				},
 
 				prefill = {
-					---@type "cursorWord"| false
 					normal = "cursorWord",
-					---@type "selectionFirstLine"| false does not work with ex-command (see README).
 					visual = "selectionFirstLine",
 					startInReplaceLineIfPrefill = false,
 					alsoPrefillReplaceLine = false,
 				},
 
-				keymaps = { -- normal & visual mode, if not stated otherwise
+				keymaps = {
 					abort = "q",
 					confirm = "<CR>",
 					insertModeConfirm = "<C-CR>",
 					prevSubst = "<Up>",
 					nextSubst = "<Down>",
-					toggleFixedStrings = "<C-f>", -- ripgrep's `--fixed-strings`
-					toggleIgnoreCase = "<C-c>", -- ripgrep's `--ignore-case`
+					toggleFixedStrings = "<C-f>",
+					toggleIgnoreCase = "<C-c>",
 					openAtRegex101 = "R",
 				},
 
@@ -43,26 +41,16 @@ return {
 					matchHlGroup = "IncSearch",
 					rangeBackdrop = {
 						enabled = true,
-						blend = 50, -- between 0 and 100
+						blend = 50,
 					},
 				},
 
 				regexOptions = {
 					startWithFixedStringsOn = false,
 					startWithIgnoreCase = false,
-					-- pcre2 enables lookarounds and backreferences, but performs slower
 					pcre2 = true,
-					-- disable if you use named capture groups (see README for details)
 					autoBraceSimpleCaptureGroups = true,
 				},
-
-				editingBehavior = {
-					-- When typing `()` in the `search` line, automatically adds `$n` to the
-					-- `replace` line.
-					autoCaptureGroups = true,
-				},
-
-				notificationOnSuccess = true,
 			}
 		end
 	},
