24a2e3cc7b61c4ca194ea3bbe95259d7a6c9de2d
Katve <eliot.fredman@proton.me>
2026-02-07 12:58:20 +0200

    Improved auto pairs

--- lua/katve/autocmds.lua
+++ lua/katve/autocmds.lua
@@ -79,8 +79,8 @@ end
 
 local prev_char = nil
 local prev_fired = nil
-local prev_row = 0
-local prev_col = 0
+local prev_open = nil
+local prev_row, prev_col = unpack(vim.api.nvim_win_get_cursor(0))
 
 local function handle_next_key(_, typed)
 	if typed == "\r" then
@@ -90,35 +90,45 @@ local function handle_next_key(_, typed)
 		vim.api.nvim_buf_set_lines(0, prev_row - 1, prev_row, false, { before_cursor, after_cursor })
 	elseif typed == vim.api.nvim_replace_termcodes("<BS>", true, false, true) then
 		vim.api.nvim_win_set_cursor(0, { prev_row, prev_col + 1 })
+		vim.schedule_wrap(vim.api.nvim_put)({ prev_char }, "c", true, true)
 	elseif typed == " " then
-		vim.api.nvim_win_set_cursor(0, { prev_row, prev_col - 1 })
-		vim.api.nvim_put({ " " }, "c", true, false)
+		vim.api.nvim_buf_set_text(0, prev_row, prev_col, prev_row, prev_col, {prev_open})
 	end
+
 	vim.on_key(nil, vim.api.nvim_get_current_buf())
 end
 
 autocmd("InsertCharPre", {
-	group = augroup("AutoPairs", { clear = true }),
+	group = augroup("CustomAutoPairs", { clear = true }),
 	callback = function()
 		local char = vim.v.char
 		local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-		local last_text = vim.api.nvim_buf_get_text(0, prev_row - 1, prev_col, prev_row - 1, prev_col + 1, {})[1]
+		local lines = vim.api.nvim_buf_get_lines(0, prev_row - 1, prev_row, false)
+		local last_line = lines[1]
+		local last_char = ""
+
+		if last_line ~= nil and #last_line or 0 >= prev_col + 1 then
+			last_char = last_line:sub(prev_col + 1, prev_col + 1)
+		end
+
 		if prev_fired == true then
 			prev_fired = false
 		elseif row == prev_row and col == prev_col + 1 then
 			for open, close in pairs(char_pairs_map) do
-				if char == open and prev_char == open and char == last_text then
+				if char == open and prev_char == open and char == last_char then
 					vim.v.char = ""
 					vim.schedule(function()
 						vim.api.nvim_win_set_cursor(0, { row, col - 1 })
-						vim.api.nvim_put({ close }, "", true, false)
+						vim.api.nvim_put({ close }, "c", true, false)
 					end)
 					vim.on_key(handle_next_key, vim.api.nvim_get_current_buf())
+					prev_open = open
 					prev_fired = true
 					break
 				end
 			end
 		end
+
 		prev_char = char
 		prev_row = row
 		prev_col = col
