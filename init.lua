0687797c9b6126774b050c54a4cf7abedb962e6e
Katve <eliot.fredman@proton.me>
2026-02-28 10:54:48 +0200

    The current implementation is functional but still need some major work

--- init.lua
+++ init.lua
@@ -1,6 +1,28 @@
 require("katve.functions")
 require("katve.lazy")
-require("katve.katpack")
+
+
+require("katve.katpack").setup({
+	configs = "config",
+	async_build = true,
+	auto_delete = true,
+	auto_update = true,
+	confirm = { install = false, update = false, delete = true },
+	prefer_config_file = true,
+})
+
+require("katve.katpack").add({
+	"gh:atiladefreitas/dooing",
+	"gh:danymat/neogen",
+	"gh:williamboman/mason.nvim",
+	"gh:brenoprata10/nvim-highlight-colors",
+	"gh:stevearc/conform.nvim",
+	"gh:mfussenegger/nvim-lint",
+	"gh:NeogitOrg/neogit",
+	"gh:nvim-mini/mini.icons",
+	"gh:HiPhish/rainbow-delimiters.nvim"
+})
+
 require("katve")
 require("config")
 ColorMyPencils()
