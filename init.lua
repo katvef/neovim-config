2553d86951e61f9d0b2446bac76eefc006568fa3
Katve <eliot.fredman@proton.me>
2026-02-28 12:28:22 +0200

    COnfig changes

--- init.lua
+++ init.lua
@@ -6,19 +6,19 @@ require("katve.katpack").setup({
 	configs = "config",
 	async_build = true,
 	auto_delete = true,
-	auto_update = true,
-	confirm = { install = false, update = false, delete = true },
+	auto_update = false,
+	confirm = { install = false, update = false },
 	prefer_config_file = true,
 })
 
 require("katve.katpack").add({
-	"gh:atiladefreitas/dooing",
+	{ src = "gh:atiladefreitas/dooing", config = function() require("dooing").setup() end },
+	{ src = "gh:NeogitOrg/neogit",      config = function() require("neogit").setup() end },
 	"gh:danymat/neogen",
 	"gh:williamboman/mason.nvim",
 	"gh:brenoprata10/nvim-highlight-colors",
 	"gh:stevearc/conform.nvim",
 	"gh:mfussenegger/nvim-lint",
-	"gh:NeogitOrg/neogit",
 	"gh:nvim-mini/mini.icons",
 	"gh:HiPhish/rainbow-delimiters.nvim"
 })
