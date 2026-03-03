b8326c3ea35429c67274303b2afaad10f2b2f852
Katve <eliot.fredman@proton.me>
2026-03-03 22:09:13 +0200

    Changed to new treesitter setup and commented out the config until I figure out how the new one works

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,22 +1,22 @@
-require("nvim-treesitter.configs").setup {
-	ensure_installed = { "lua", "markdown", "markdown_inline", "comment" },
-	sync_install = false,
-	indent = { enable = true },
-	install_info = { only_install = true },
-	auto_install = true,
-
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "gnn",
-			node_incremental = "grn",
-			node_decremental = "grm",
-			scope_incremental = "grc",
-		},
-	},
-
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = true,
-	},
+require("nvim-treesitter").setup {
+	-- ensure_installed = { "lua", "markdown", "markdown_inline", "comment" },
+	-- sync_install = false,
+	-- indent = { enable = true },
+	-- install_info = { only_install = true },
+	-- auto_install = true,
+	--
+	-- incremental_selection = {
+	-- 	enable = true,
+	-- 	keymaps = {
+	-- 		init_selection = "gnn",
+	-- 		node_incremental = "grn",
+	-- 		node_decremental = "grm",
+	-- 		scope_incremental = "grc",
+	-- 	},
+	-- },
+	--
+	-- highlight = {
+	-- 	enable = true,
+	-- 	additional_vim_regex_highlighting = true,
+	-- },
 }
