eca2b6ab9c0e7b9ee95aa05e9f932568046ad78e
Kati Fredman <kati@katvef.fi>
2026-07-16 03:35:11 +0300

    Changed flash binds

--- lua/config/flash.lua
+++ lua/config/flash.lua
@@ -8,14 +8,9 @@ require("flash").setup({
 	}
 })
 
-vim.keymap.set({ "n", "x", "o" }, "<M-C-s>", function() require("flash").jump() end, { desc = "Flash" })
-vim.keymap.set({ "n", "x", "o" }, "š", function() require("flash").jump() end, { desc = "Flash" })
 vim.keymap.set({ "n", "x", "o" }, "ß", function() require("flash").jump() end, { desc = "Flash" })
 
-vim.keymap.set({ "n", "x", "o" }, "<M-C-S-s>", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
-vim.keymap.set({ "n", "x", "o" }, "Š", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
 vim.keymap.set({ "n", "x", "o" }, "ẞ", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
-vim.keymap.set({ "o", "x" }, "<C-r>", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
+vim.keymap.set({ "n", "x", "o" }, "<C-s>", function() require("flash").treesitter() end, { desc = "Toggle Flash Search" })
 
 vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
-vim.keymap.set("n", "<C-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
