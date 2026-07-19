054270673166477938a5921e87c54d8ba9fdeec9
Kati Fredman <kati@katvef.fi>
2026-07-19 21:18:53 +0300

    Don't autoinstall treesitter parsers

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,12 +1,8 @@
 require("nvim-treesitter").setup()
 
-local parsers = require("nvim-treesitter.parsers")
-
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(args)
 		local lang = vim.treesitter.language.get_lang(args.match)
-
-		if parsers[lang] then require("nvim-treesitter").install({ lang }):wait(10000) end
 
 		if lang and vim.treesitter.language.add(lang) then
 			vim.treesitter.start(args.buf, lang)
